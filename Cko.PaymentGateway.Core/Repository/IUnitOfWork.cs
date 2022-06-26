2b1a1df122d2cd37013a53981a2eeb628fe56ceb
Gafar Popoola <gafar.popoola@m-kopa.com>
2022-06-26 10:28:23 +0100

    Add unit of work

---
+++ Cko.PaymentGateway.Core/Repository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Cko.PaymentGateway.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cko.PaymentGateway.Core.Repository
+{
+    public interface IUnitOfWork
+    {
+        IBaseRepository<Transaction> TransactionRepository { get; set; }
+        void Dispose();
+        Task Save();
+    }
+}
